237dc31d0b8390ebf60a3484de7007a531a2ccb7
Nasirilahi <nasirilahi.ilahi2@yahoo.com>
2017-07-20 22:45:18 +0530

    added header on webview

--- src/components/views/ArticleWebView/ArticleWebView.js
+++ src/components/views/ArticleWebView/ArticleWebView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {
     StyleSheet,
     View,
@@ -17,5 +17,15 @@ const ArticleWebView = ({navigation})=>{
             />
     );
 };
+ArticleWebView.navigationOptions = ({ navigation }) => ({
+         title: `Articles Web View`,
+         headerStyle:{backgroundColor:'goldenrod'},
+         headerTitleStyle:{color:'white', fontSize:24,},
+         gesturesEnabled:false,
+         headerLeft:( <TouchableOpacity style={{marginLeft:10}} onPress={() => navigation.goBack()}>
+                            <Icon name="md-arrow-back" size={30} color="#FFF" />
+                    </TouchableOpacity>
+        ),
+});
 
 export default ArticleWebView;
